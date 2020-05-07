96a2538fd8c631b8dc8ca59d5f39b833e212688e
Gilang Prasetia <prameswara888@gmail.com>
2020-05-07 12:48:22 +0700

    React Redux Twitter clone

---
+++ src/components/Dashboard.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Tweet from './Tweet'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className='center'>Your Timeline</h3>
+        <ul className='dashboard-list'>
+          {this.props.tweetIds.map((id) => (
+            <li key={id}>
+              <Tweet id={id}/>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+// 1. conect react redux dapet data dari app.js
+// componentDidMount() {
+//   this.props.dispatch(handleInitialData())
+// }
+// 2. langsung ke mapstate to props
+// 3. di consume component
+
+function mapStateToProps ({ tweets }) {
+  return {
+    tweetIds: Object.keys(tweets)
+      .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
